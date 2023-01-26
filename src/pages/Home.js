4c332df7e72b701d425ca469896fa7bd4a4546d2
robbeca <xmfkddl1006@gmail.com>
2023-01-26 18:14:21 +0900

    Created a Sidebar layout.

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import cn from 'classnames';
+import style from './Home.module.css';
+
+function Home() {
+  return(
+    <section className={cn(style.grid)}>
+    </section>
+  );
+}
+
+export default Home;
